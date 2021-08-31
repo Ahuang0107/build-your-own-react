f07eddc09b8461245a9bcd67266d14a5b59091a5
Elase Huang <elase.huang@outlook.com>
2021-08-31 09:50:37 +0800

    create self react 2.0

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,24 @@
 import React from './react.js'
+import ReactDOM from "./react-dom.js";
 
 /** @jsx React.createElement */
-const element = <h1 title="foo">Hello</h1>
+const element = (
+    <div id="foo" className={'foo'}>
+        <a>level-01 bar</a>
+        <b>level-01 foz</b>
+        <c>
+            <div id="foo" className={'foo'}>
+                <a>level-02 bar</a>
+                <b>level-02 foz</b>
+                <c>
+                    <div id="foo" className={'foo'}>
+                        <a>level-03 bar</a>
+                        <b>level-03 foz</b>
+                    </div>
+                </c>
+            </div>
+        </c>
+    </div>
+)
 const container = document.getElementById("root")
-
-const node = document.createElement(element.type)
-node.title = element.props.title
-
-const text = document.createTextNode("")
-text.nodeValue = element.props.children
-
-node.appendChild(text)
-container.appendChild(node)
+ReactDOM.render(element, container)
