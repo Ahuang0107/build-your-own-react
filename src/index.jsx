63124182356b2995b6935bb4945d09863adf76a4
Elase Huang <elase.huang@outlook.com>
2021-09-01 20:32:51 +0800

    create self react 4.0 [reconciliation]

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,19 @@
 import React from './react.js'
 import ReactDOM from "./react-dom.js";
-
 /** @jsx React.createElement */
-const element = (
-    <div id="foo" className={'foo'}>
-        <a>level-01 bar</a>
-        <b>level-01 foz</b>
-        <c>
-            <div id="foo" className={'foo'}>
-                <a>level-02 bar</a>
-                <b>level-02 foz</b>
-                <c>
-                    <div id="foo" className={'foo'}>
-                        <a>level-03 bar</a>
-                        <b>level-03 foz</b>
-                    </div>
-                </c>
-            </div>
-        </c>
-    </div>
-)
+
+const updateValue = e => {
+    rerender(e.target.value)
+}
+
+const rerender = value => {
+    const element = (
+        <div>
+            <input onInput={updateValue} value={value} />
+            <h2>Hello {value}</h2>
+        </div>
+    )
+    ReactDOM.render(element, container)
+}
 const container = document.getElementById("root")
-ReactDOM.render(element, container)
+rerender("World")
